296da00bbf1ca2df9e28aa259318cd6bd27d3496
Yann DEBLY <yann.debly@reseau.eseo.fr>
2020-10-04 10:06:53 +0200

    run_compose cleaned up

--- run_compose.sh
+++ run_compose.sh
@@ -2,12 +2,5 @@
 # LAMP et Wordpress
 # Yann Debly 2020
 
-#On récupère Wordpress si ce n'est pas déjà fait
-#if [ -d "./wordpress" ]; then
-    #echo "Getting Wordpress ..."
-   # sudo wget https://fr.wordpress.org/latest-fr_FR.tar.gz
-  #  echo "Extracting Wordpress ..."
- #   sudo tar -xvf *.tar.gz
-#fi
 echo "Starting services ..."
 sudo docker-compose up -d --build
